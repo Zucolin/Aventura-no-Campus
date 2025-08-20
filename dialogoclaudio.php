196286644a4d5a208ad0051c2cb6c89db0e3d750
Vtr-Cardoso69 <vitor.c.lemes@aluno.senai.br>
2025-08-20 18:46:06 -0300

    P7

--- dialogoclaudio.php
+++ dialogoclaudio.php
@@ -8,16 +8,33 @@
 </head>
 <body>
     
+
+
 <button id="btn-personagem" class="personagem" style="background: none; border: none; padding: 0; cursor: pointer;">
     <img src="img/claudio.png.png" alt="Personagem" style="width: 100px; height: auto; display: block;">
 </button>
 
+<button id="btn-personagem" class="personagem1" style="background: none; border: none; padding: 0; cursor: pointer;">
+    <img src="img/vitor.png" alt="Personagem" style="width: 80px; height: auto; display: block;">
+</button>
+
+
 
 
 <div id="caixa-mensagem" class="mensagem">
-	<span class="msg-avatar1"></span>
-	<span class="msg-text">CLAUDIO: Olá! Eu sou o Cláudio. Clique para ver o diálogo aparecer igual ao desafio 1.</span>
+    <span class="msg-avatar1"></span>
+    <span class="msg-text">CLAUDIO: Harammm te peguei inferior, mecehendo no celular? me entregue isso agora!!!</span>
 </div>
+
+<div id="caixa-mensagem2" class="mensagem" style="visibility:hidden">
+    <span class="msg-avatar" style="background-image: url('img/vitor.png');"></span>
+    <span class="msg-text">VITOR: Calma, professor! Eu só estava vendo o horário da aula.</span>
+</div>
+
+
+
+
+
 
 <script>
 	const btnPersonagem = document.getElementById('btn-personagem');
@@ -28,7 +45,13 @@
 			caixaMensagem.classList.remove('mostrar');
 		}, 7000); // 7 segundos igual ao desafio 1
 	});
-</script>
+    
+    
+    
+    
+    
+    
+    </script>
 
 </body>
 </html>
@@ -73,16 +96,29 @@
 			background-position: center;
 			background-repeat: no-repeat;
 		}
-		.personagem {
-			position: absolute;
-			top: 50%;
-			left: 70%; 
-			transform: translate(-50%, -50%);
-			width: 100px;
-			height: auto;
-		transition: transform 0.3s ease; 
-        }
-		.personagem:hover {
+            .personagem {
+                position: absolute;
+                top: 35%;
+                left: 57%; 
+                transform: translate(-50%, -50%);
+                width: 100px;
+                height: auto;
+            transition: transform 0.3s ease; 
+            }
+
+          .personagem1 {
+                position: absolute;
+                top: 60%;
+                left: 70%; 
+                transform: translate(-50%, -50%);
+                width: 100px;
+                height: auto;
+            transition: transform 0.3s ease; 
+            }
+        .personagem1 img {
+    transform: scaleX(-1);
+}
+        .personagem:hover {
 			transform: translate(-50%, -50%) scale(1.2);
 		}
 
